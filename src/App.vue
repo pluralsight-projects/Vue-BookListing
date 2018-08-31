c3d6b456b10ced340514ffcb8af69077705946f2
Arlen Neylon <aneylon@scramsystems.com>
2018-08-31 15:46:11 -0600

    Add basic booklist.

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
-    <hello-world></hello-world>
+    <book-list></book-list>
+    <!-- <hello-world></hello-world> -->
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld';
+import BookList from './components/BookList';
 
 export default {
   name: 'app',
   components: {
-    HelloWorld,
+    BookList,
+    // HelloWorld,
   },
 };
 </script>
