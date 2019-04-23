c7aeab0bc16a45f0795ddfa49e112a2c69035305
cureham <poncho.correa@gmail.com>
2019-04-23 15:53:29 -0500

    Completed the Module
    Completed the Module

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -1,38 +1,39 @@
 <template>
   <div>
     <h1>{{title}}</h1>
-    <ul>      
-      <li v-for="book in books">{{book.title}}:{{book.author}}</li>      
+    <ul>
+      <li v-for="book in books">{{book.title}}:{{book.author}}</li>
     </ul>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'BookList',
-  data(){
-    return{
-      title:'All Books',
-      books:[
-        {title: 'Self Reliance', author:'Ralph Waldo Emerson'},
-        {title: 'American Gods', author:'Neil Gaiman'},
-        {title: 'Amusing Ourselves to Death', author:'Neil Postman'}
+  name: "BookList",
+  data() {
+    return {
+      title: "All Books",
+      books: [
+        { title: "Self Reliance", author: "Ralph Waldo Emerson" },
+        { title: "American Gods", author: "Neil Gaiman" },
+        { title: "Amusing Ourselves to Death", author: "Neil Postman 2" }
       ]
-    }
+    };
   }
-}
+};
 </script>
 
 <style>
-  h1, h2{
-    font-weight: normal;
-  }
-  ul{
-    list-style-type:none;
-    padding:0;
-  }
-  li{
-    display:block;
-    margin:0 10px;
-  }
+h1,
+h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: block;
+  margin: 0 10px;
+}
 </style>
