33269add01d4b6347eec2e1d5f111e3f8ef233b0
Aim <bintghazall@gmail.com>
2020-12-07 17:21:29 +0100

    sorting some mismatch

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -1,22 +1,22 @@
 <template>
     <div>
-        <h1>{{title}}</h1>
-    </div>
+    <h1>{{title}}</h1>
+    </div>    
 </template>
 
 <script>
-
-
 export default {
-    name: 'BookList',
+    name: "BookList",
     data(){
         return{
-            title:"All Books"
+            title: "All Books"
         }
     }
 }
 </script>
 
 <style>
-
+    h1,h2{
+        font-weight: normal;
+    }
 </style>
