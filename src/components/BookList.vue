62aae6554337811480554bd4a23155c342f2764b
Jonathan Schmaltz <jonathan.schmaltz@bitmain.com>
2021-01-08 11:33:33 -0500

    Added loop for the array of books, css, and list elements

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -2,6 +2,10 @@
 <template>
   <div>
       <h1>{{ title }}</h1>
+      <ul>
+        <li v-for="book in books">{{book.title}} : {{book.author}}</li>
+      </ul>
+
   </div>
 </template>
 
@@ -10,7 +14,12 @@
     name:"BookList",
       data() {
         return {
-          title: 'All Books'
+          title: 'All Books',
+          books: [
+            {title: 'Self-Reliance', author: 'Ralph Waldo Emerson'},
+            {title: 'American Gods', author: 'Neil Gaiman'},
+            {title: 'Amusing Ourselves to Death', author: 'Neil Postman'},
+]
       };
     }
 
@@ -22,4 +31,13 @@
 h1, h2 {
   font-weight: normal;
 }
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: block;
+  margin: 0 10px;
+}
+
 </style>
