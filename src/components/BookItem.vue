098bfc4350228f69cc40f24538ee51f1195fe2d6
Jonathan Schmaltz <jonathan.schmaltz@bitmain.com>
2021-01-08 14:02:00 -0500

    Created child component

---
+++ src/components/BookItem.vue
@@ -0,0 +1,27 @@
+// JavaScript source code
+<template>
+  <div>
+      <li>{{book.title}} : {{book.author}}</li>
+  </div>
+</template>
+
+<script>
+  export default {
+    name:"BookItem",
+    props: ["book"],
+      data() {
+        return {
+
+      };
+    }
+
+
+  };
+</script>
+
+<style>
+li {
+  display: block;
+  margin: 0 10px;
+}
+</style>
